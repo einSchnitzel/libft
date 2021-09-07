0edd89f9885c465518ee61eb08cb5f43ae662192
smetzler <smetzler@smetzlers-MacBook-Air.local>
2021-09-07 14:27:41 +0200

    ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 10:59:36 by smetzler          #+#    #+#             */
-/*   Updated: 2021/08/10 16:34:37 by smetzler         ###   ########.fr       */
+/*   Updated: 2021/09/07 14:26:35 by smetzler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
 	char	*helper;
-
+	char	letter;
+	
 	i = ft_strlen(s);
 	helper = (char *)s;
+	letter = (char)c;
 	while (helper[i] != s[0])
 	{
-		if (helper[i] == c)
+		if (helper[i] == letter)
 		{
-			return (&helper[i]);
+			return (helper + i);
 		}
 		i--;
 	}
-	if (helper[i] == c)
-		return (&helper[i]);
+	if (helper[i] == letter)
+		return (helper + i);
 	return (NULL);
 }
